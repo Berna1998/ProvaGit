49d7ecfb7118dfa6f654c88c1209a395a5293665
Berna1998 <francy.1927@hotmail.it>
2023-03-25 17:38:13 +0100

    Versione 3, stampa hello world e 33

--- HelloWorld/src/logic/Hello.java
+++ HelloWorld/src/logic/Hello.java
@@ -5,8 +5,8 @@ public class Hello {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello world");
-		System.out.println("How are you?");
-
+		System.out.println("33");
+	
 	}
 
 }
